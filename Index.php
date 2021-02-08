a30f615a09812eaeb282be749b3d957ee8e55783
denisarebecca <77790645+denisarebecca@users.noreply.github.com>
2021-02-08 17:04:50 +0800

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,12 +1,12 @@
 <html>
-<Head> <title>COVID 19</title> </head>
+<H1> <title>COVID 19</title> </H1>
         	<Body><body style = background-color:#6495ED>
-<Header><p align = "center"> <font size = "70" face = "Copperplate gothic bold" color = "black"><b><u>Covid-19</u></b></font>
+<H4><p align = "center"> <font size = "70" face = "Copperplate gothic bold" color = "black"><b><u>Covid-19</u></b></font></H4>
 <br><p align = "center"><img src = "lol.jpg" Width = "820"  Length = "440">
 <br><p align = "left"><Font size = "6"><Font color = "black"><body style = background-color:#FCF8EC>
 What is Covid 19?
 <p align = "left"><Font size = "4"><Font color = "black"> <Font face = "Castellar">
-Coronavirus disease (COVID-19),
+Coronavirus disease (COVID-19),<br>
 <br>is an infectious disease caused by a newly discovered coronavirus.
 <br>Most people who fall sick with COVID-19 will experience mild to moderate symptoms and  
 <br>recover without special treatment. In early 2020, following an outbreak in China in 
